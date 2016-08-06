c3ed3f5b112a363ee6fa9d6beb96ceeb46eeb93e
cafeunder <cafe3641@gmail.com>
2016-08-07 04:04:54 +0900

    16パズルを作ります

---
+++ Solution/project/web/fileManager.js
@@ -0,0 +1,95 @@
+﻿"use strict";
+
+var fileManager = new FileManager();
+//-------------------------------------//
+//             FileManager             //
+//-------------------------------------//
+function FileManager(){
+	//以下に読み込む画像を指定します。
+	//読み込みのタイミングが同じファイル同士をグループ化してください。	
+	var manifests = {
+		"main" : [
+			{"id":"fig", "src":"img/sepia.png"},
+		],
+	};
+
+	//ファイルグループの作成
+	this.__fileGroup = new Object();
+	for(var key in manifests){
+		this.__fileGroup[key] = new FileGroup(manifests[key]);
+	}
+}
+
+FileManager.prototype.load = function(groupID){
+	halt(this.__fileGroup[groupID] == undefined);
+	this.__fileGroup[groupID].load();
+}
+
+FileManager.prototype.unload = function(groupID){
+	halt(this.__fileGroup[groupID] == undefined);
+	this.__fileGroup[groupID].unload();
+}
+
+FileManager.prototype.isLoaded = function(groupID){
+	halt(this.__fileGroup[groupID] == undefined);
+	return this.__fileGroup[groupID].loaded;
+}
+
+FileManager.prototype.getResult = function(groupID, fileID){
+	halt(this.__fileGroup[groupID] == undefined);
+	return this.__fileGroup[groupID].getResult(fileID);
+}
+
+FileManager.prototype.getProgress = function(groupID){
+	halt(this.__fileGroup[groupID] == undefined);
+	return this.__fileGroup[groupID].progress();
+}
+
+FileManager.prototype.getGroup = function(groupID){
+	halt(this.__fileGroup[groupID] == undefined);
+	return this.__fileGroup[groupID];
+}
+
+//------------------------------------//
+//              FileGroup             //
+//------------------------------------//
+function FileGroup(manifest){
+	this.loaded = false;
+	this.__lock = false;
+	this.__manifest = manifest;
+	this.__queue = new createjs.LoadQueue(true);	
+
+	var instance = this;
+	this.__queue.addEventListener("complete", function(){ instance.loaded = true; });
+	this.__queue.addEventListener("progress", function(){ console.log("Progress:", instance.__queue.progress); });
+}
+
+//ロードを開始するメソッド
+FileGroup.prototype.load = function(){
+	if(this.__lock) { return; }
+	this.__lock = true;
+
+	this.__queue.loadManifest(this.__manifest, true);
+}
+
+//ロードしたファイルを削除するメソッド
+FileGroup.prototype.unload = function(){
+	this.__queue.removeAll();
+	this.__lock = false;
+	this.loaded = false;
+}
+
+//ロード状態を返すメソッド
+FileGroup.prototype.progress = function(){
+	return this.__queue.progress;
+}
+
+//ロードしたファイルを返すメソッド
+FileGroup.prototype.getResult = function(fileID){
+	halt(!this.loaded);
+
+	var result = this.__queue.getResult(fileID);
+	halt(result == null);
+
+	return result;
+}
---
+++ Solution/project/web/program.js
@@ -0,0 +1,81 @@
+﻿"use strict";
+
+//キャンバスの横幅と縦幅
+var STAGE_HEIGHT, STAGE_WIDTH;
+//読み込みが完了したか？
+var loadComplete = false;
+
+onload = function(){
+	//ステージの生成（引数はキャンバスID）
+	var stage = new createjs.Stage("myCanvas");
+
+	//マウスオブジェクトの生成（以下をセットで呼び出すこと）
+	MouseInitialize(stage);
+
+	//タッチデバイスかどうか
+	if(createjs.Touch.isSupported() == true){
+		//タッチデバイスの有効化（引数はステージオブジェクト）
+		createjs.Touch.enable(stage)
+	} else {
+		//マウスオーバーを有効にする
+		//マルチデバイスではあまり推奨されない
+		stage.enableMouseOver();
+	}
+	
+	//ステージの横幅と縦幅の取得
+	STAGE_WIDTH = stage.canvas.width;
+	STAGE_HEIGHT = stage.canvas.height;
+
+	//=====背景の生成=====//
+	//シェイプオブジェクトの生成
+	var backGround = new createjs.Shape();
+	//ぬりつぶす（引数は色）
+	backGround.graphics.beginFill("#fff");
+	//四角形とする（引数はx,y,width,height）
+	backGround.graphics.drawRect(0, 0, STAGE_WIDTH, STAGE_HEIGHT);
+	//ステージに追加
+	stage.addChild(backGround);
+
+	//fps表示
+	var fpsLabel = new createjs.Text("-- fps", "bold 18px Arial", "#000");
+	stage.addChild(fpsLabel);
+
+	//FPSベース
+	createjs.Ticker.setFPS(30);
+	createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
+
+	var fileGroup = fileManager.getGroup("main");
+	fileGroup.load();
+
+	//タイマーイベント
+	var bool = false;
+	var bmp = null;
+	createjs.Ticker.on("tick", function () {
+		//mouseアップデート
+		mouse.update();
+
+		//更新メソッド
+		if(!bool && fileGroup.loaded){
+			bmp = new createjs.Bitmap(fileGroup.getResult("fig"));
+			stage.addChild(bmp);
+			bool = true;
+		}
+
+		//fps計測
+		fpsLabel.text = Math.round(createjs.Ticker.getMeasuredFPS());
+
+		stage.update();
+	});
+};
+
+function copyArray(arr){
+	var newarr = [];
+	for(var i = 0; i < arr.length; i++){
+		if(Array.isArray(arr[i])){
+			newarr[i] = copyArray(arr[i]);
+		} else {
+			newarr[i] = arr[i];
+		}
+	}
+	return newarr;
+}
